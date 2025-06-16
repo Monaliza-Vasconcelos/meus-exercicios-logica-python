25910ce2debe35c88a47790b9ec98ed4683b4252
Monaliza Vasconcelos <monalizavasconcelos889@gmail.com>
2025-06-16 10:25:31 -0300

    Create ex005_media_duas_notas.py

---
+++ ex005_media_duas_notas.py
@@ -0,0 +1,4 @@
+Nota1 = float(input('Digite a primeira nota: '))
+Nota2 = float(input('Digite a segunda nota: '))
+Media = float((Nota1+Nota2)/2)
+print('A média entre {} e {} é igual a {} '.format(Nota1,Nota2,Media))
