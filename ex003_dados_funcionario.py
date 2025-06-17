6f9ceb4be32ff1fd07eedb2f692e041b6fdbf1d8
Monaliza Vasconcelos <monalizavasconcelos889@gmail.com>
2025-06-17 12:58:57 -0300

    Update ex003_dados_funcionario.py

--- ex003_dados_funcionario.py
+++ ex003_dados_funcionario.py
@@ -1,3 +1,5 @@
+# ex003_dados_funcionario.py
+# Programa que solicita o nome e o salário de um(a) funcionário(a) e exibe essas informações formatadas na tela.
 nome = str(input('Qual o seu nome? '))
 salario = float (input('Qual seu salário? R$'))
 print ('Nome do(a) funcionário(a): {} '.format(nome))
