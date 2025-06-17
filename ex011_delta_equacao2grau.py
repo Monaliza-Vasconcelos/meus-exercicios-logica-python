1e9ea3a0f9fcf9016b38144dc1dd0d4d5c028c94
Monaliza Vasconcelos <monalizavasconcelos889@gmail.com>
2025-06-17 12:08:18 -0300

    Create ex011_delta_equacao2grau.py

---
+++ ex011_delta_equacao2grau.py
@@ -0,0 +1,5 @@
+A = float (input('Digite o valor de A: '))
+B = float (input('Digite o valor de B: '))
+C = float (input('Digite o valor de C: '))
+delta = float (B**2)-4*A*C
+print('O valor de delta é: {}'.format(delta))
