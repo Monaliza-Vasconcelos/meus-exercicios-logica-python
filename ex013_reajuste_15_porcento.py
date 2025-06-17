6475bfa47ca2dcb00539c36d1d0f7844a06b276c
Monaliza Vasconcelos <monalizavasconcelos889@gmail.com>
2025-06-17 12:20:38 -0300

    Create ex013_reajuste_15_porcento.py

---
+++ ex013_reajuste_15_porcento.py
@@ -0,0 +1,3 @@
+salario_funcionario = float(input('Qual seu salário? '))
+aumento_15_porcento = float(15/100*salario_funcionario)
+print('Seu novo salário com 15% de aumento será: R$ {}'.format(salario_funcionario+aumento_15_porcento))
