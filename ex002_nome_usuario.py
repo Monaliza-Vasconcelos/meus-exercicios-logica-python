cb8b1e1fc286585bd214d804925c745d50306f56
Monaliza Vasconcelos <monalizavasconcelos889@gmail.com>
2025-06-17 12:58:02 -0300

    Update ex002_nome_usuario.py

--- ex002_nome_usuario.py
+++ ex002_nome_usuario.py
@@ -1,2 +1,4 @@
+# ex002_nome_usuario.py
+# Programa que solicita o nome do usuário e exibe uma mensagem personalizada de boas-vindas.
 nome = str (input('Digite seu nome: '))
 print('Olá {}, é um prazer te conhecer!'.format(nome))
